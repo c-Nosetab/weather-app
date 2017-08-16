1f5e61e1a84b5ff3d5145e808a1881d169c9017a
c-Nosetab <cpbateson@gmail.com>
2017-08-16 15:46:25 -0500

    fixin things up

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -6,44 +6,62 @@ document.addEventListener('DOMContentLoaded', function(event) {
       zipcode: '',
       location: '',
       weatherReport: [],
-      cache: {}
+
+      lastSearch: 0,
+      loading: false,
+      cache: {},
+      errors: []
 
 
     },
 
     methods: {
 
+      tooFast: function(now) {
+        if (this.lastSearch + 2000 > now) {
+          this.errors = ['You\'re requesting too often! Wait at least 2 seconds'];
+        } else {
+          this.errors = [];
+          this.searchZipcode()
+        };
+      },
+
       searchZipcode: function() {
+        this.errors = ['Zipcode cannot be blank.', 'Zipcode must be 5 numbers. IE: 60622' ]
+
         if (this.zipcode != '') {
           this.weatherReport = [];
+          this.loading = true;
+          this.errors = []
+          this.lastSearch = Date.now()
 
           if (this.fetch(this.zipcode)) {
             var weatherInfo = this.fetch(this.zipcode)
 
             for (var i = 0; i < 3; i++) {
-                this.weatherReport.push(weatherInfo[i]);
+              this.weatherReport.push(weatherInfo[i]);
             };
 
             this.zipcode = '';
+            this.loading = false;
           } else {
             $.get('http://api.wunderground.com/api/624c587a66ec60af/forecast/q/' + this.zipcode +'.json', function(list) {
 
-              this.store(this.zipcode, list['forecast']['simpleforecast']['forecastday']);
-
-
-              for (var i = 0; i < 3; i++) {
-                this.weatherReport.push(list['forecast']['simpleforecast']['forecastday'][i]);
+              if (list['forecast']) {
+                this.store(this.zipcode, list['forecast']['simpleforecast']['forecastday']);
+                for (var i = 0; i < 3; i++) {
+                  this.weatherReport.push(list['forecast']['simpleforecast']['forecastday'][i]);
+                };
+              } else if (list['response']){
+                this.errors = ['Zipcode must be 5 numbers. IE: 60622', list['response']['error']['description']]
               };
+
               this.zipcode = '';
-            }.bind(this)).fail(function(response) {
-              console.log('nope');
-            }.bind(this));
+              this.loading = false;
+
+            }.bind(this))
           }
         }
-      },
-
-      showCache: function() {
-        console.log(localStorage);
       },
 
       store: function(zipcode, weather) {
@@ -65,11 +83,10 @@ document.addEventListener('DOMContentLoaded', function(event) {
           } else {
             localStorage.removeItem(zipcode);
             return null;
-          }
-
+          };
         } else {
           return null;
-        }
+        };
       }
     },
 
